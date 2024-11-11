f076d3ab44a39bb0babb9996d27da32858038476
雷子 <jianlei.chen@lucahealthcare.com>
2024-11-11 11:22:08 +0800

    test_log1

--- packages/luca_components/lib/luca_components.js
+++ packages/luca_components/lib/luca_components.js
@@ -6,5 +6,13 @@ function test_log() {
   console.log('I am v0.0.6 of lucaComponents');
 }
 
+function test_log1() {
+  console.log('I am v0.0.6 of lucaComponents - 1');
+}
+
 'use strict';
-module.exports = { lucaComponents, test_log };
+module.exports = {
+  lucaComponents,
+  test_log,
+  test_log1,
+};
